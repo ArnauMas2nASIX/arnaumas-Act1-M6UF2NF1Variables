a2293028911cd3469d1c0db80a8661c9f0ce22c0
ArnauMas2nASIX <158025106+ArnauMas2nASIX@users.noreply.github.com>
2024-01-29 10:57:12 +0100

    Update arnaumas_EX1.sh

--- arnaumas Act2 M6UF2NF1 Condicionals/arnaumas_EX1.sh
+++ arnaumas Act2 M6UF2NF1 Condicionals/arnaumas_EX1.sh
@@ -6,9 +6,13 @@
 # comparant-ho amb zero.
 # ----------------------------------------------------------------------------------------
 
-# Demanem el nombre a l'usuari
+# Demanem el nombre a l'usuari, especificant amb el paràmetre -n que és un nombre
 echo -n "Entra un nombre enter: "
+# El guardem en una variable
 read num
+
+# Fem la comparació amb el 0. Si és més gran, ens sortirà que és positiu. En cas contrari, que
+# el nombre és negatiu i és un zero
 if [[ $num -gt 0 ]]
 then
 	echo "El nombre és positiu"
