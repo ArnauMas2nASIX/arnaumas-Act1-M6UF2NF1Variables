6615edf26fc60730aea4467810afcc9d56477a31
ArnauMas2nASIX <158025106+ArnauMas2nASIX@users.noreply.github.com>
2024-01-29 10:54:12 +0100

    Update arnaumas_EX1.sh

--- arnaumas Act2 M6UF2NF1 Condicionals/arnaumas_EX1.sh
+++ arnaumas Act2 M6UF2NF1 Condicionals/arnaumas_EX1.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+# ENUNCIAT
+# Ús de condicionals amb operadors
+# Realitza un script que rebi un nombre enter per teclat i digui si és positiu o negatiu,
+# comparant-ho amb zero.
+# ----------------------------------------------------------------------------------------
+
+# Demanem el nombre a l'usuari
+echo -n "Entra un nombre enter: "
+read num
+if [[ $num -gt 0 ]]
+then
+	echo "El nombre és positiu"
+else
+	echo "El nombre és negatiu o zero"
+fi
