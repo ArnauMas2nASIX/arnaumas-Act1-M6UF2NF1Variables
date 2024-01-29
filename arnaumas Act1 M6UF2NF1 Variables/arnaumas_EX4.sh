4d68f8c2704c590b2fdb57060df5ca06fcfc6a62
ArnauMas2nASIX <158025106+ArnauMas2nASIX@users.noreply.github.com>
2024-01-29 10:06:58 +0100

    Update arnaumas_EX4.sh

--- arnaumas Act1 M6UF2NF1 Variables/arnaumas_EX4.sh
+++ arnaumas Act1 M6UF2NF1 Variables/arnaumas_EX4.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
 
+# ENUNCIAT
+# Realitza un script que rebi diversos arguments per, a continuació, mostrar (i explicar)
+# la diferència entre utilitzar $* i $@.
+# -------------------------------------------------------------------------------------------
+
+# Els arguments els passarem directament al executar l'script, posats tots
+# des d'allà (ja que si ho fem amb read no llegirà quants hi ha passats)
+
+# Aleshores, a partir dels arguments posats, definim què fa cada variable com ens demana,
+# Explicant en cada cas com funcionen
+echo "Si utilitzem el mètode de dollar + asterisc, mostrarem els arguments dins d'una sola string"
+echo "-" $*
+echo ""
+echo "Si utilitzem el mètode dollar + arroba, els arguments estaran dins d'una array"
+echo "-" $@
+echo ""
+
+# Mostrem les Conclusions per pantalla
+echo "Com es pot comprovar, a l'hora de mostrar-los per pantalla es veuen igual,"
+echo "però els scripts ho tracten de manera diferent a l'hora de processar-los"
