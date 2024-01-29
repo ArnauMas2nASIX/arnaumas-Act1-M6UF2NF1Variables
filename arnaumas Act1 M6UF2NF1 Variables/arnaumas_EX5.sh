8c1b0c500a25d8693e06875c8406b94c31a4497d
ArnauMas2nASIX <158025106+ArnauMas2nASIX@users.noreply.github.com>
2024-01-29 10:16:15 +0100

    Update arnaumas_EX5.sh

--- arnaumas Act1 M6UF2NF1 Variables/arnaumas_EX5.sh
+++ arnaumas Act1 M6UF2NF1 Variables/arnaumas_EX5.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+# ENUNCIAT
+# Crea un script que demani a l’usuari que introdueixi un nom d’usuari i una contrasenya per,
+# a continuació, mostrar-ho tot en una sola línia.
+# Quan l’usuari introdueixi la seva contrasenya, aquesta no ha d’aparèixer.
+# ---------------------------------------------------------------------------------------------
+
+# Demanem el nom d'usuari
+read -p "Introdueix el nom d'usuari: " name_user
+read -s -p "Introdueix la contrasenya: " pass_user
+
+# I després, ho mostrem per pantalla, però només el nom d'usuari
+echo "" # Per afegir un salt de línia
+echo "" # Per afegir una línia en blanc per a més estètica de l'output
+echo "Nom d'usuari nou:" $name_user
+echo "Contrasenya:" $pass_user
