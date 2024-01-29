de526a66f13937b144d2e1b6a0695b6dbde237c6
ArnauMas2nASIX <158025106+ArnauMas2nASIX@users.noreply.github.com>
2024-01-29 09:21:52 +0100

    Update arnaumas_EX1.sh

--- arnaumas Act1 M6UF2NF1 Variables/arnaumas_EX1.sh
+++ arnaumas Act1 M6UF2NF1 Variables/arnaumas_EX1.sh
@@ -1,3 +1,23 @@
-# Crida de variables en Bash de Linux
-# Realitza un script que mostri per pantalla les principals variables globals i els,
-# tots els arguments del Shell, el valor retornat per la darrera funció o comanda i el PID de la Shell actual.
+#!/bin/bash
+
+# ENUNCIAT
+# Realitza un script que mostri per pantalla pes principals variables globals i els,
+# tots els arguments del Shell, el valor retornat per la darrera funció o comanda
+# i el PID de la Shell actual.
+# --------------------------------------------------------------------------------------
+
+# Mostrem la variable que ens diu a quin directori HOME treballem
+echo "El teu directori HOME és:" $HOME
+
+# Mostrem la variable que ens diu a quins directoris buscarà aquest script
+# els diferents arxius que hauria d'executar
+echo "El directori on l'script buscarà fitxers per executar és:" $PATH
+
+# Mostrem la SHELL en la que estem treballant
+echo "La teva SHELL és:" $SHELL
+
+# Mostrem el nom d'usuari
+echo "El teu nom d'usuari és:" $USERNAME
+
+# Mostrem el directori actual de l'usuari
+echo "El teu directori actual és:" $PWD
